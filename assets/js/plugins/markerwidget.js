7d38bf907bf7ad92d43a02ec9e06d1979bd4572f
py0777 <py0777@naver.com>
2020-12-04 22:45:01 +0900

    dd

--- assets/js/plugins/markerwidget.js
+++ assets/js/plugins/markerwidget.js
@@ -1,12 +1,12 @@
-(function(d, h, m){
-    var js, fjs = d.getElementsByTagName(h)[0];
-    if (d.getElementById(m)){return;}
-    js = d.createElement(h); js.id = m;
-    js.onload = function(){
-        window.makerWidgetComInit({
-        position: "right",          
-        widget: "8mnpqgeqiyruagr3-jgcvvdkqr35fuyox-whwv9asjyonauycg"                
-    })};
-    js.src = "https://makerwidget.com/js/embed.js";
-    fjs.parentNode.insertBefore(js, fjs)
-}(document, "script", "dhm"));
+    (function(d, h, m){
+        var js, fjs = d.getElementsByTagName(h)[0];
+        if (d.getElementById(m)){return;}
+        js = d.createElement(h); js.id = m;
+        js.onload = function(){
+            window.makerWidgetComInit({
+            position: "right",          
+            widget: "dx5p2s27mdnfdqe3-9tfg8qqmkvzvjw3x-3mwyt9f0tlym2gcv"                
+        })};
+        js.src = "https://makerwidget.com/js/embed.js";
+        fjs.parentNode.insertBefore(js, fjs)
+    }(document, "script", "dhm"));
